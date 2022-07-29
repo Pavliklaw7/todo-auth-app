2ccaf6df08f72450eb0f4faf0423704303f84602
Vladyslav Pavlik <pavliklaw7@gmail.com>
2022-07-29 19:59:57 +0300

    deploy.sh fix

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:username/todo-auth-app.git master:gh-pages
+git push -f git@github.com:Pavliklaw7/todo-auth-app.git master:gh-pages
 cd -
